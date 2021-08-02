f8d8c4aa72208184796d7aa723061f5dd2e76d26
Kohei Otsuka <kochizufan@gmail.com>
2021-08-03 05:48:27 +0900

    Update git switch script

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 const gulp = require("gulp");
 const fs = require("fs-extra");
 
-gulp.task("git_keep", async () => {
-  fs.removeSync('.git');
-  fs.copySync('.git.keep', '.git');
-});
-
-gulp.task("git_open", async () => {
-  fs.removeSync('.git');
-  fs.copySync('.git.open', '.git');
+gulp.task("git_switch", async () => {
+  try {
+    fs.statSync('.git.open');
+    fs.moveSync('.git', '.git.keep');
+    fs.moveSync('.git.open', '.git');
+  } catch(e) {
+    fs.moveSync('.git', '.git.open');
+    fs.moveSync('.git.keep', '.git');
+  }
 });
