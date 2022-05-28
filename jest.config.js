cd85365e54561f3d84d6c821f127012d3a9cc4d6
Kohei Otsuka <kochizufan@gmail.com>
2022-05-28 13:44:21 +0900

    Change delaunator as a logic

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest/presets/js-with-babel',
   transform: {
-    "^.+\\.ts$": "ts-jest"
+    "^.+\\.ts$": "ts-jest",
+    "delaunator": "babel-jest",
+    "constrainautor": "babel-jest",
+    "robust-predicates": "babel-jest"
   },
+  "transformIgnorePatterns": ["/node_modules/(?!(delaunator|@kninnug/constrainautor|robust-predicates)/)"]
 };
