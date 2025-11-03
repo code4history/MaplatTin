d756c262fb6d5757832bd7456d7ba44d6ab599be
Kohei Otsuka <kochizufan@gmail.com>
2025-11-03 17:56:38 +0900

    Update cjs interface

--- dist/index.cjs
+++ dist/index.cjs
@@ -3,4 +3,5 @@ const pkg = require("./maplat_tin.cjs");
 const Tin = pkg && pkg.default ? pkg.default : pkg;
 module.exports = Tin;
 Object.assign(module.exports, pkg);
+Object.defineProperty(module.exports, "__esModule", { value: true });
 module.exports.default = Tin;
--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ const cjsCompatWrapper = () => ({
         "const Tin = pkg && pkg.default ? pkg.default : pkg;",
         "module.exports = Tin;",
         "Object.assign(module.exports, pkg);",
+        'Object.defineProperty(module.exports, "__esModule", { value: true });',
         "module.exports.default = Tin;"
       ].join("\n")
     });
